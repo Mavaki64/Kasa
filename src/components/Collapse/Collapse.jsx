41ece9d4b3c1577093d7f54b75a196749ab521c7
Killian <killiangayez@gmail.com>
2026-01-04 13:12:43 +0100

    Feat: Intégration de la page à propos
    - Ajout de la bannière sur la page
    - Ajout des collapse menu a partir d'un tableau d'objets
    - Modification du composant collapse pour le rendre cohérent au besoin
    - Ajout du css mobile first + responsive
    - Ajout des animations a l'ouverture et la fermeture des collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import './Collapse.scss'
 
 export default function Collapse({ title, content, size = 'large' }) {
+	const [isOpen, setIsOpen] = useState(false)
+
 	return (
 		<div className={`collapse collapse--${size}`}>
-            <div className={`collapse__header collapse__header--${size}`}> 
-                <h3 className={`collapse__title collapse__title--${size}`}>{title}</h3>
-                <i className={`fa-solid fa-chevron-up collapse__icon collapse__icon--${size}`}></i>
-            </div>
-			
-			<div className={`collapse__content collapse__content--${size}`}>{content.map((item) => <p key={item}>{item}</p>)}</div>
+			<button
+				className={`collapse__header collapse__header--${size}`}
+				onClick={() => setIsOpen(!isOpen)}
+				type="button">
+				<h3 className={`collapse__title collapse__title--${size}`}>{title}</h3>
+				<i
+					className={`fa-solid fa-chevron-up collapse__icon collapse__icon--${size} ${
+						isOpen ? 'collapse__icon--open' : ''
+					}`}></i>
+			</button>
+
+			<div className={`collapse__content collapse__content--${size} ${isOpen ? 'collapse__content--open' : ''}`}>
+				<p className="collapse__content-text">{content}</p>
+			</div>
 		</div>
 	)
 }
