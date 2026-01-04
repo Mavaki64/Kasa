0c16de1c1984c750a1b6e58bd4b628451c17130b
Killian <killiangayez@gmail.com>
2026-01-04 14:10:25 +0100

    Feat : Ajout de la page 404 erreur et du style

--- src/pages/public/Error/Error.jsx
+++ src/pages/public/Error/Error.jsx
@@ -1,8 +1,14 @@
 import './Error.scss'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
 	return (
-		<>
-		</>
+		<div className="error-container">
+			<h1 className="error404">404</h1>
+			<h2 className="error-text">Oups! La page que<span>vous demandez n'existe pas.</span></h2>
+			<Link to="/" className="error-return-link">
+				Retourner sur la page d'accueil
+			</Link>
+		</div>
 	)
 }
